550b41ccbeeee375d63e7d625751f2f1dfd0f5c2
rizkysafdila <rizkysafdila@gmail.com>
2022-11-13 16:59:11 +0700

    init project

---
+++ resources/views/dashboard/layouts/main.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elegant Dashboard | Dashboard</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./img/svg/logo.svg" type="image/x-icon">
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <div class="layer"></div>
+    <!-- ! Body -->
+    <a class="skip-link sr-only" href="#skip-target">Skip to content</a>
+    <div class="page-flex">
+        <!-- ! Sidebar -->
+        @include('dashboard.partials.sidebar')
+        <div class="main-wrapper">
+            <!-- ! Main nav -->
+            @include('dashboard.partials.navbar')
+            <!-- ! Main -->
+            <main class="main users chart-page" id="skip-target">
+                @yield('content')
+            </main>
+            <!-- ! Footer -->
+            @include('dashboard.partials.footer')
+        </div>
+    </div>
+    <!-- Chart library -->
+    <script src="./plugins/chart.min.js"></script>
+    <!-- Icons library -->
+    <script src="plugins/feather.min.js"></script>
+    <!-- Custom scripts -->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -0,0 +1,68 @@
+<aside class="sidebar">
+    <div class="sidebar-start">
+        <div class="sidebar-head">
+            <a href="/" class="logo-wrapper" title="Home">
+                <span class="sr-only">Home</span>
+                <span class="icon logo" aria-hidden="true"></span>
+                <div class="logo-text">
+                    <span class="logo-title">Elegant</span>
+                    <span class="logo-subtitle">Dashboard</span>
+                </div>
+            </a>
+            <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
+                <span class="sr-only">Toggle menu</span>
+                <span class="icon menu-toggle" aria-hidden="true"></span>
+            </button>
+        </div>
+        <div class="sidebar-body">
+            <ul class="sidebar-body-menu">
+                <li>
+                    <a class="active" href="/"><span class="icon home" aria-hidden="true"></span>Dashboard</a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        <span class="icon paper" aria-hidden="true"></span>
+                        Laporan Struk
+                    </a>
+                    <span class="msg-counter">7</span>
+                </li>
+            </ul>
+            <ul class="sidebar-body-menu">
+                <li>
+                    <a class="show-cat-btn" href="a">
+                        <span class="icon user-3" aria-hidden="true"></span>Users
+                        <span class="category__btn transparent-btn" title="Open list">
+                            <span class="sr-only">Open list</span>
+                            <span class="icon arrow-down" aria-hidden="true"></span>
+                        </span>
+                    </a>
+                    <ul class="cat-sub-menu">
+                        <li>
+                            <a href="/">Users-01</a>
+                        </li>
+                        <li>
+                            <a href="/">Users-02</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/"><span class="icon setting" aria-hidden="true"></span>Settings</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="sidebar-footer">
+        <a href="##" class="sidebar-user">
+            <span class="sidebar-user-img">
+                <picture>
+                    <source srcset="./img/avatar/avatar-illustrated-01.webp" type="image/webp"><img src="./img/avatar/avatar-illustrated-01.png" alt="User name">
+                </picture>
+            </span>
+            <div class="sidebar-user-info">
+                <span class="sidebar-user__title">Nafisa Sh.</span>
+                <span class="sidebar-user__subtitle">Support manager</span>
+            </div>
+        </a>
+    </div>
+</aside>
